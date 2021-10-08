c59c7afa815353447a92ddbacb816b6d2bd66206
Envimaster@combat <Schoenb8@staff.uni-marburg.de>
2021-10-09 01:11:34 +0200

    Init Package src
    Main Package information

---
+++ R/PAMcorrection-Package.R
@@ -0,0 +1,15 @@
+#' PAMcorrection
+#'
+#' @name PAMcorrection - package
+#' @docType package
+#' @title PAMcorrection - a tool for PAM correction
+#' @description The \code{PAMcorrection} package is used to estimate and test correction vlaues for the PAM using control values.
+#' @author Andreas Schönberg
+#' @keywords package
+NULL
+#' @docType data
+#' @name lau_mspec - data
+#' @title Example Dataset
+#' @description A data.frame containing PAM and control values
+#' @format \code{"utils::read.csv"}
+NULL
