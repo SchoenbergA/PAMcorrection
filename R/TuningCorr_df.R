4a1458466f6fde6f6a19a3674ac2841beb87e1f1
Envimaster@combat <Schoenb8@staff.uni-marburg.de>
2022-01-21 00:17:40 +0100

    Correcdtions

--- R/TuningCorr_df.R
+++ R/TuningCorr_df.R
@@ -5,15 +5,15 @@
 #' use the global mean to correct PAM.
 #' @param att2 character - Additional column name of data frame which should be used to correct by mean.
 #' Only necessary if the correction should use subgroups for the data in 'pos1'
-#' @param tuning character - method for clipping the data used to calculate the mean. Methods can be "whisker", "quantil" or "treshold". If 'NULL' will perform no tuning. Default= NULL.
-#' @param treshold numeric - specifies the treshold to clip values for tuning. Only needed if 'tuning' == "treshold".
+#' @param tuning character - method for clipping the data used to calculate the mean. Methods can be "whisker", "quantil" or "threshold". If 'NULL' will perform no tuning. Default= NULL.
+#' @param threshold numeric - specifies the threshold to clip values for tuning. Only needed if 'tuning' == "threshold".
 #' @return returns a data.frame with an additional column "PAM_corr" with the corrected PAM values
 #' and prints the respective mean values for each class or combination of classes. Uses 'tuned' data to calculate the mean values.
 #' @note If used without any attributes will use the global mean over all values to correct PAM. IF 'att1' is set
 #' will calculate the mean for each group in 'att1' to correct PAM. IF 'att2' is given will use the mean for each group
 #' with the combination for each attribute.
-#' @details * tuning - available modes are "whisker" (which clips all values </> lower / upper whisker treshold in boxplot) and "quantil" (which clips all values </> 25 and 75 quantils).
-#' "treshold" will clip all values > a specified treshold.
+#' @details * tuning - available modes are "whisker" (which clips all values </> lower / upper whisker threshold in boxplot) and "quantil" (which clips all values </> 25 and 75 quantils).
+#' "threshold" will clip all values > a specified threshold.
 #' * If a specific class or combination of attributes has no values left after tuning the mean will be calculated based on the original data. This will lead to a warning.
 #' The class or combination is marked as 'noData' in the result table.
 #' * If a class or combination has a 'NaN' the class or combination is not existing in the dataframe. This is possible when using att1 and att2 an if not every att1 has all att2 classes.
@@ -39,14 +39,14 @@
 #'
 #' # use one class (no tuning)
 #' corrected <-TuningCorr_df(dat,att1 = "dilect")
-#' # use one class with treshold tuning
-#' corrected <-TuningCorr_df(dat,att1 = "dilect",tuning="treshold",treshold=0.8)
+#' # use one class with threshold tuning
+#' corrected <-TuningCorr_df(dat,att1 = "dilect",tuning="threshold",threshold=0.8)
 #' # use two classes (means for each generation depending on dilect) tuned by 'whisker'
 #' corrected <-TuningCorr_df(dat,att1 = "generation",att2 = "dilect",tuning = "whisker")
 
 
 
-TuningCorr_df <-function(df,att1=NULL,att2=NULL,tuning=NULL,treshold=NULL){
+TuningCorr_df <-function(df,att1=NULL,att2=NULL,tuning=NULL,threshold=NULL){
 
   # check input
   if(is.null(att1) & is.null(att2)==F){
@@ -85,13 +85,13 @@ TuningCorr_df <-function(df,att1=NULL,att2=NULL,tuning=NULL,treshold=NULL){
     dft <-dft[!(dft$diffa<bp$stats[2,1] | dft$diffa>bp$stats[4,1]),] # clip out of quantil
     cat(paste0("Tuning data by ",tuning),sep="\n")
     cat(paste0("clipping ",nrow(df)-nrow(dft), " entries",sep="\n"))
-  } else if(tuning=="treshold"){
+  } else if(tuning=="threshold"){
     # copy df
     dft <- df
     # get absolut difference
     dft$diffa <- abs(dft$PAM-dft$CTR)
-    dft <-dft[!(dft$diffa>treshold),] # clip out of whisker
-    cat(paste0("Tuning data by treshold ",treshold),sep="\n")
+    dft <-dft[!(dft$diffa>threshold),] # clip out of whisker
+    cat(paste0("Tuning data by threshold ",threshold),sep="\n")
     cat(paste0("clipping ",nrow(df)-nrow(dft), " entries",sep="\n"))
   }
 
